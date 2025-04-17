e81b86a6a5d9d43147c914ba4398b93bf05fc15b
Wet-Senker <dhrnickdevries@hotmail.com>
2025-04-17 10:45:46 +0200

    add triangle shape for player to screen

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2, player_radius=PLAYER_RADIUS)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-        screen.fill("black")
-        pygame.display.flip()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
